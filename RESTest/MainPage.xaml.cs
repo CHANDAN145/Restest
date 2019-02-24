8fea995ca6ac39b836cb11bb6c3c25db7d7a74b2
Chandan Pradeep <Chandan.Pradeep@kas-services.com>
2019-02-24 16:30:57 +0530

    intial commit with ListView

---
+++ RESTest/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RESTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            List<User> Users = new List<User>();
+            User user1 = new User
+            {
+                name = "Vidya",
+                id = 0
+            };
+            User user2 = new User
+            {
+                name = "Chandan",
+                id = 1
+            };
+
+            Users.Add(user1);
+            Users.Add(user2);
+            EmployeeList.ItemsSource = Users;
+        }
+    }
+}
