4b5d3a9169e0212d3f0e95d6eb7e10b1abc3d120
Chandan Pradeep <Chandan.Pradeep@kas-services.com>
2019-02-24 17:08:10 +0530

    Adding HTTPclient for getting list of users

--- RESTest/MainPage.xaml.cs
+++ RESTest/MainPage.xaml.cs
@@ -4,29 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
-
+using Newtonsoft.Json;
 namespace RESTest
 {
     public partial class MainPage : ContentPage
     {
+        List<User> Users = new List<User>();
+
         public MainPage()
         {
             InitializeComponent();
-            List<User> Users = new List<User>();
-            User user1 = new User
-            {
-                name = "Vidya",
-                id = 0
-            };
-            User user2 = new User
-            {
-                name = "Chandan",
-                id = 1
-            };
+            //User user1 = new User
+            //{
+            //    name = "Vidya",
+            //    id = 0
+            //};
+            //User user2 = new User
+            //{
+            //    name = "Chandan",
+            //    id = 1
+            //};
 
-            Users.Add(user1);
-            Users.Add(user2);
+            //Users.Add(user1);
+            //Users.Add(user2);
+        }
+        protected async override void OnAppearing()
+        {
+            Service service = new Service();
+            string result = await service.GetAsyc("https://jsonplaceholder.typicode.com/users");
+            Console.WriteLine(result);
+            Users = JsonConvert.DeserializeObject<List<User>>(result);
+
             EmployeeList.ItemsSource = Users;
+
+            base.OnAppearing();
         }
     }
 }
---
+++ RESTest/Service.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RESTest
+{
+    public class Service
+    {
+        static HttpClient client = new HttpClient();
+
+        public async Task<string> GetAsyc(string path)
+        {
+            HttpResponseMessage response = await client.GetAsync(path);
+            List<User> users = new List<User>();
+            string data = string.Empty;
+            if (response.IsSuccessStatusCode)
+            {
+                data = await response.Content.ReadAsStringAsync();
+            }
+
+            return data;
+        }
+    }
+}
